0cb225510baa061d89a91c89612231e364ac386b
Mauricio Chiapetta <m.faisca1234@gmail.com>
2024-02-27 23:29:55 -0300

    commit

--- script.js
+++ script.js
@@ -31,7 +31,14 @@ const locations = [
     "button text":["Buy 10 health (10 gold)","Buy weapon (30 gold)","Go to town square"],
     "button functions":[buyHealth,buyWeapon,goTown],
     text:"You enter the store."
+},
+{
+    name:"cave",
+    "button text":["Fight slime","Fight fanged beast","Go to town square"],
+    "button functions":[fightSlime,fightBeast,goTown],
+    text:"You enter the cave. You see some monsters."
 }
+
 ]
 
 // initialize buttons
@@ -58,27 +65,40 @@ function goTown() {
 }
 
 function goStore() {
-    
+
     update(locations[1])
 
+
+}
+function goCave() {
+   
+    update(locations[2])
 
 }
 
 
 function buyHealth() {
 
+    gold -=10;
+    health  +=10
+
+    goldText.innerText = gold
+    healthText.innerText = health
+    
+    
 }
 
 function buyWeapon() {
 
 }
 
-
-
-function goCave() {
-    console.log("Going to cave.");
+function fightSlime(){
 
 }
+function  fightBeast(){
+
+}
+
 
 function fightDragon() {
     console.log("Fighting dragon.")
