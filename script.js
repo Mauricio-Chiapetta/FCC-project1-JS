db3b245845603d42dcb439ff98d120b15d9844c3
Mauricio Chiapetta <m.faisca1234@gmail.com>
2024-03-12 23:31:44 -0300

    final commit

--- script.js
+++ script.js
@@ -308,26 +308,22 @@ function pick(guess) {
   text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
 
   for (let i = 0; i < 10; i++) {
-    text.innerText += numbers[i] + "\n"
+    text.innerText += numbers[i] + "\n";
 
-    if(numbers.includes(guess)){
-
-      text.innerText +="Rigth! you win 20 gold!"
-      gold+=20
-      goldText.innerText = gold
-
-    }else{
-      text.innerText +="Wrong! You lose 10 health!"
-      health-=10
-      healthText.innerText = health
+    if (numbers.includes(guess)) {
+      text.innerText += "Rigth! you win 20 gold!";
+      gold += 20;
+      goldText.innerText = gold;
+    } else {
+      text.innerText += "Wrong! You lose 10 health!";
+      health -= 10;
+      healthText.innerText = health;
     }
-    if(health <= 0){
-      lose()
+    if (health <= 0) {
+      lose();
     }
-
   }
 }
-
 
 function pickTwo() {
   pick(2);
